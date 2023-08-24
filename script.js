bce2c004ee9809300ec071b217e76f4e7220eb4a
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-08-24 20:20:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function nextPalindrome(num) {
   //your JS code here.
+	for(let i = ++num;i<=1000000;i++) {
+		let s = i+"";
+		let flag = 0;
+		for(let j = 0;j<s.length/2;j++) {
+			if(s.charAt(j)!==s.charAt(s.length-j-1)) {
+				flag = 1;
+				break;
+			}
+		}
+		if(flag == 0) {
+			return i;
+		}
+	}  
 }
 
 const input = prompt("Enter a palindrome number");
